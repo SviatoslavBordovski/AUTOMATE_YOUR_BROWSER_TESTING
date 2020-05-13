f4605d5504bdde894f57441465dd787f093a73af
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-05-13 20:08:07 +0300

    Update chrome.py

--- tests/chrome.py
+++ tests/chrome.py
@@ -28,15 +28,3 @@ driver.find_element_by_name('btnK').click()
 
 #Take care, sweety!
 driver.quit()
-
-#FIREFOX BROWSER TEST
-
-driver = webdriver.Firefox()  #Don't forget to install gecko driver anb tell the path to it on the laptop (required)
-driver.get('https://google.com')
-driver.find_element_by_name('q').send_keys('Python is awesome')
-wait = WebDriverWait(driver, 10)
-element = wait.until(EC.element_to_be_clickable((By.NAME, 'btnK')))
-
-driver.find_element_by_name('btnK').click()
-driver.close()
-driver.quit()
