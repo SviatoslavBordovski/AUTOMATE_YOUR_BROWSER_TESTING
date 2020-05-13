23605bc386d6c9cdbcd72c6d4b15bc1289b58457
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-05-13 22:13:46 +0300

    Update firefox.py

--- tests/firefox.py
+++ tests/firefox.py
@@ -1,4 +1,4 @@
-frm selenium import webdriver
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
@@ -8,11 +8,19 @@ from selenium.webdriver.support import expected_conditions as EC
 #FIREFOX BROWSER TEST
 
 driver = webdriver.Firefox(executable_path='your\path\geckodriver')  #Don't forget to install gecko driver anb tell the path to it on the laptop (required)
-driver.get('https://google.com')
-driver.find_element_by_name('q').send_keys('Python is awesome')
-wait = WebDriverWait(driver, 10)
-element = wait.until(EC.element_to_be_clickable((By.NAME, 'btnK')))
 
+#Where do we go?
+driver.get('https://google.com')
+
+#Find the input field on the page and type the text that should be found
+driver.find_element_by_name('q').send_keys('Python is a programming language')
+wait = WebDriverWait(driver, 10)
+
+#Click find on the button 'Google search'
+element = wait.until(EC.element_to_be_clickable((By.NAME, 'btnK')))
 driver.find_element_by_name('btnK').click()
+
+#Finish test
+print('Test completed!')
 driver.close()
 driver.quit()
