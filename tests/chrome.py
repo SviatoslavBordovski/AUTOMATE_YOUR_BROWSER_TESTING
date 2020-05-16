7781ce32096eaf8b007b03da2bd80a92533f2476
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-05-16 22:33:28 +0300

    Update chrome.py

--- tests/chrome.py
+++ tests/chrome.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support import expected_conditions as EC
 
-#Tell the path to chromedriver on your laptop like in the example below (required)
+#Tell the path to ChromeDriver on your laptop like in the example below (required)
 driver = webdriver.Chrome('/home/sb/Desktop/chromedriverlinux/chromedriver')
 
 #Lets set browser window size for test run
@@ -17,14 +17,14 @@ driver.get('https://google.com')
 #What do we find and type?
 driver.find_element_by_name('q').send_keys('Python is awesome')
 
-#Oh wait! This should be clickable, so we need to wait for a while ...
+#Oh wait! Element on the webpage should be clickable, so we need to wait for a while ...
 wait = WebDriverWait(driver, 10)
 element = wait.until(EC.element_to_be_clickable((By.NAME, 'btnK')))
 
 #Click me, geeez!
 driver.find_element_by_name('btnK').click()
 
-#Finish test
+#Finish this test
 print('Chrome UI test completed!')
 driver.close()
 driver.quit()
